53bc905d61fb70cc0b04dedab185641bdbe2fdfd
Erbium84 <148142619+Erbium84@users.noreply.github.com>
2024-08-09 22:56:19 +0530

    Add files via upload

---
+++ exp1.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+class info{
+    public:
+        string name;
+        char surname;
+        int marks;
+        char gender;
+        char result;
+    info(){
+        cout<<"Enter name"<<endl;
+        cin>>name;
+        cout<<"Enter surname"<<endl;
+        cin>>surname;
+        cout<<"Enter marks"<<endl;
+        cin>>marks;
+        cout<<"Enter gender"<<endl;
+        cin>>gender;
+        cout<<"Name and surname: "<<name<<" "<<surname<<endl;
+        if(marks>33) result='P';
+        else result='F';
+        cout<<"result: "<<result<<endl;
+    }
+};
+int main(){
+    info s1;
+}
